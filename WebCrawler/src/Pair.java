da054bd353c82a7618f460eb54ff10cd12d54658
Henry Garcia <hgarc014@ucr.edu>
2015-02-07 20:16:18 -0800

    Added vaiables and get functions

--- WebCrawler/src/Pair.java
+++ WebCrawler/src/Pair.java
@@ -1,3 +1,18 @@
-public class Pair{
+public class Pair(){
 	//hello julia
+    private int hops;
+    private String urlLink;
+
+    public Pair(int num, String url){
+        hops = num;
+        urlLink = url;
+    }
+
+    public int getNumHops(){
+        return num;
+    }
+
+    public String getLink(){
+        return urlLink;
+    }
 }
